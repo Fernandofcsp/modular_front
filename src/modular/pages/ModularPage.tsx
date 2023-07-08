ca666fd0c09dfd61296ebbb97466606ae3de7a41
Angel Zapata Marquez <angel.zapata.developer@gmail.com>
2023-07-07 22:14:26 -0600

    Tailwind (Styles): I added tailwind styles

--- src/modular/pages/ModularPage.tsx
+++ src/modular/pages/ModularPage.tsx
@@ -1,20 +1,25 @@
-import { userStore } from "../../store/userStore";
+import { userStore } from '../../store/userStore';
 import { useNavigate } from 'react-router-dom';
 
 export const ModularPage = () => {
-  const { id, name, rol, token } = userStore((state) => state );
-  const logout = userStore(state => state.logout);
-  const navigate = useNavigate();
-  return (
-    <div>
-      <p>Tu ID: { id }</p>
-      <p>Nombre: { name }</p>
-      <p>Rol: { rol }</p>
-      <p>Token: { token }</p>
-      <button onClick={() => {
-        logout()
-        navigate('/login')
-      }}>Logout</button>
-    </div>
-  )
-}
+	const { id, name, rol, token } = userStore((state) => state);
+	const logout = userStore((state) => state.logout);
+	const navigate = useNavigate();
+	return (
+		<div>
+			<p>Tu ID: {id}</p>
+			<p>Nombre: {name}</p>
+			<p>Rol: {rol}</p>
+			<p>Token: {token}</p>
+			<button
+				className='bg-green-500 px-3 py-2 rounded-[16px]'
+				onClick={() => {
+					logout();
+					navigate('/login');
+				}}
+			>
+				Logout
+			</button>
+		</div>
+	);
+};
