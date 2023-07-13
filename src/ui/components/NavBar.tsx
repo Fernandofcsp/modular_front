6cf2411c760781eb6ee5a990069de94ffff29be9
Fernando Cesar Sandoval Padilla <fernando.sandoval6854@alumnos.udg.mx>
2023-07-12 18:03:54 -0600

    Navbar button darkmode
    changued icon to toggle dark and light mode

--- src/ui/components/NavBar.tsx
+++ src/ui/components/NavBar.tsx
@@ -3,9 +3,7 @@ import { userStore } from "../../store/userStore";
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { useNavigate } from "react-router-dom";
-import profile from "../../../public/assets/icons/profile.png"
-import Bsun from "../../../public/assets/icons/sun.png";
-import Bmoon from "../../../public/assets/icons/moon.png";
+import profile from "../../../public/assets/images/thispersondoesnotexist.jpg";
 import hand from "../../../public/assets/icons/mano.png";
 
 const classNames = (...classes: string[]) => {
@@ -16,7 +14,7 @@ const NavBar = () => {
   const { name } = userStore((state) => state);
   const logout = userStore((state) => state.logout);
   const navigate = useNavigate();
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState("light");
   useEffect(() => {
     theme === "dark"
       ? document.documentElement.classList.add("dark")
@@ -34,11 +32,15 @@ const NavBar = () => {
       <div className="flex flex-row items-center gap-x-5">
         {theme === "dark" ? (
           <button onClick={togleDarkMod} style={{ width: "20px", height: "20px",}}>
-              <img src={Bsun} />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+              </svg>
           </button>
         ) : (
           <button onClick={togleDarkMod} style={{ width: "20px", height: "20px",}}>
-            <img src={Bmoon} />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+              </svg>
           </button>
         )}
         {/*Contenedor */}
