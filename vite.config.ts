f0e8a9c44fb5d9a52a3003621b22d55bc30c403b
Fernando Cesar Sandoval Padilla <fernando.sandoval6854@alumnos.udg.mx>
2024-03-15 13:46:19 -0600

    Deploy 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Especifica el directorio de salida de la compilación
+    outDir: 'dist', // Especifica el directorio de salida de la compilación
   },
 })
